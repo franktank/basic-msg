8ff4cb9cf8faf48775734f49c4277cb59a709833
Franky Liang <fliangz96@gmail.com>
2017-06-02 17:42:03 -0400

    Change IPs in producer

--- producer.rb
+++ producer.rb
@@ -10,7 +10,7 @@ class Producer
     @port = port
     @consumer_ports = consumer_ports
     @udp_socket = UDPSocket.new
-    @udp_socket.bind("127.0.0.1", port)
+    @udp_socket.bind(local_ip, port)
   end
 
   def local_ip
@@ -35,13 +35,14 @@ class Producer
   def contact_port(message)
     p @consumer_ports
     @consumer_ports.each do |cp|
-      @udp_socket.send message, 0, "127.0.0.1", cp
+      @udp_socket.send message, 0, local_ip, cp
     end
   end
 
   def receive_port_broadcast
     broadcast_socket = UDPSocket.new
-    broadcast_socket.bind("0.0.0.0", 2900) # 0.0.0.0 is the address to receive broadcasts
+    l_ip = (local_ip.split('.')[0..2]+['255']).join('.')
+    broadcast_socket.bind(l_ip, 2900) # 0.0.0.0 is the address to receive broadcasts
     begin # emulate blocking recvfrom, handle exception if no message, retry until receives one
       p port_num = broadcast_socket.recvfrom_nonblock(1024)[0]  #=> ["aaa", ["AF_INET", 33302, "localhost.localdomain", "127.0.0.1"]]
     rescue IO::WaitReadable
