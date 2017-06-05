354f68edc6da65361cb616e8d83e7b123955888e
Franky Liang <fliangz96@gmail.com>
2017-06-05 13:08:12 -0400

    WIP

--- producer.rb
+++ producer.rb
@@ -41,9 +41,9 @@ class Producer
 
   def receive_port_broadcast
     broadcast_socket = UDPSocket.new
-    l_ip = (local_ip.split('.')[0..2]+['255']).join('.')
-    p l_ip
-    broadcast_socket.bind(l_ip, 2900) # 0.0.0.0 is the address to receive broadcasts
+    # l_ip = (local_ip.split('.')[0..2]+['255']).join('.')
+    # p l_ip
+    broadcast_socket.bind('0.0.0.0', 2900) # 0.0.0.0 is the address to receive broadcasts
     begin # emulate blocking recvfrom, handle exception if no message, retry until receives one
       p port_num = broadcast_socket.recvfrom_nonblock(1024)[0]  #=> ["aaa", ["AF_INET", 33302, "localhost.localdomain", "127.0.0.1"]]
     rescue IO::WaitReadable
